c52804fd477f09b13c1b1c93807d896641255db4
Paul-Adrien Cordonnier <pauladrien@adaltas.com>
2025-04-08 16:55:42 +0200

    feat: add change dir in init script

--- onyxia-init.sh
+++ onyxia-init.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+# Définir une valeur par défaut pour WORK_DIR si non défini
+WORK_DIR=${WORK_DIR:-"python-streamlit-demo"}
+
+# Tenter de se déplacer dans le dossier
+if cd "$WORK_DIR"; then
+  echo "Dans le dossier : $WORK_DIR"
+else
+  echo "Le dossier $WORK_DIR est introuvable ou inaccessible, poursuite du script..."
+fi
+
 pip install -r requirements.txt
